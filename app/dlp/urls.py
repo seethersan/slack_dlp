41fdd8dbed0c3f52dc86d7d72c0571d5e60e6d5b
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2024-10-26 12:18:23 -0500

    add api urls

--- app/dlp/urls.py
+++ app/dlp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from dlp import views
+from dlp.api import views as api_views
 
 urlpatterns = [
     path('slack/events/', views.slack_event_listener, name='slack_event_listener'),
+    path('api/patterns/', api_views.get_patterns, name='get_patterns'),
+    path('api/save_match/', api_views.save_scan_result, name='save_scan_result'),
 ]
